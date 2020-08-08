390cb7a9caf4c45084dcd019f884f445d4f280a4
lengyijun <sjtu5140809011@gmail.com>
2020-08-08 19:56:47 +0800

    FA final score

--- csv/b.py
+++ csv/b.py
@@ -6,7 +6,7 @@ def title(filename):
     mp={}
     res={}
     x=filename.split(".")[0]
-    print("const "+x+"_list=")
+    print("const "+x+"_list=",end="")
     with open(filename, encoding='utf-8') as f:
         reader = csv.reader(f)
         for row in reader:
@@ -21,8 +21,24 @@ def title(filename):
     print(res,end="")
     print(";")
 
+def final(filename):
+    res={}
+    x=filename.split(".")[0]
+    print("const "+x+"_final=",end="")
+    with open(filename, encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            season=row[0]
+            data=row[1:4]
+            year=season.split("-")[0]
+            if(int(year)>=1992):
+                res[season]=data
+    print(res,end="")
+    print(";")
 
 if __name__ == '__main__':
     title('UCL.txt')
     title('UEL.txt')
     title('CWC.txt')
+
+    final('FA.txt')
